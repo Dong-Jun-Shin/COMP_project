fc38ba7ca90c214f79f5fff7b0d8e8f478f28f9a
Dong-jun Sin <tlsehdwns239@gmail.com>
2019-11-15 17:43:47 +0900

    19/11/15
    제품조회 - 트리뷰 목록 구현, css 적용

--- Document/Code_source/COMP/src/controller/SalesWatchTabController.java
+++ Document/Code_source/COMP/src/controller/SalesWatchTabController.java
@@ -7,7 +7,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TreeCell;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -51,26 +54,92 @@ public class SalesWatchTabController implements Initializable {
 	private Button btnLast;
 	@FXML
 	private Button btnNext;
-	
+
 	private Stage primaryStage;
 
 	public void setPrimaryStage(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 	}
-	
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		Image comChildIcon = new Image(getClass().getResourceAsStream("/image/TreeView/COM.png"), 10, 10, false, false);
+		Image cpChildIcon = new Image(getClass().getResourceAsStream("/image/TreeView/CP.png"), 10, 10, false, false);
+		Image rChildIcon = new Image(getClass().getResourceAsStream("/image/TreeView/R.png"), 10, 10, false, false);
+		Image mbChildIcon = new Image(getClass().getResourceAsStream("/image/TreeView/MB.png"), 10, 10, false, false);
+		Image gChildIcon = new Image(getClass().getResourceAsStream("/image/TreeView/G.png"), 10, 10, false, false);
+		Image ssChildIcon = new Image(getClass().getResourceAsStream("/image/TreeView/SS.png"), 10, 10, false, false);
+		Image hChildIcon = new Image(getClass().getResourceAsStream("/image/TreeView/H.png"), 10, 10, false, false);
+		Image poChildIcon = new Image(getClass().getResourceAsStream("/image/TreeView/PO.png"), 10, 10, false, false);
+		Image caChildIcon = new Image(getClass().getResourceAsStream("/image/TreeView/CA.png"), 10, 10, false, false);
+		Image coChildIcon = new Image(getClass().getResourceAsStream("/image/TreeView/CO.png"), 10, 10, false, false);
+		Image swChildIcon = new Image(getClass().getResourceAsStream("/image/TreeView/SW.png"), 10, 10, false, false);
+		Image kChildIcon = new Image(getClass().getResourceAsStream("/image/TreeView/K.png"), 10, 10, false, false);
+		Image moChildIcon = new Image(getClass().getResourceAsStream("/image/TreeView/MO.png"), 10, 10, false, false);
+		Image spChildIcon = new Image(getClass().getResourceAsStream("/image/TreeView/SP.png"), 10, 10, false, false);
+		Image mnChildIcon = new Image(getClass().getResourceAsStream("/image/TreeView/MN.png"), 10, 10, false, false);
 
+		productTreeView.getStylesheets().add("/application/treeView.css");
+		productTreeView.getStyleClass().add("my-tree-view");
+
+		TreeItem<String> root = new TreeItem("제품구성", new ImageView(comChildIcon));
+		Object[] subRoot = { new TreeItem<String>("CPU", new ImageView(cpChildIcon)),
+				new TreeItem<String>("RAM", new ImageView(rChildIcon)),
+				new TreeItem<String>("MB", new ImageView(mbChildIcon)),
+				new TreeItem<String>("GPU", new ImageView(gChildIcon)),
+				new TreeItem<String>("SSD", new ImageView(ssChildIcon)),
+				new TreeItem<String>("HDD", new ImageView(hChildIcon)),
+				new TreeItem<String>("파워", new ImageView(poChildIcon)),
+				new TreeItem<String>("케이스", new ImageView(caChildIcon)),
+				new TreeItem<String>("쿨러", new ImageView(coChildIcon)),
+				new TreeItem<String>("SW", new ImageView(swChildIcon)),
+				new TreeItem<String>("키보드", new ImageView(kChildIcon)),
+				new TreeItem<String>("마우스", new ImageView(moChildIcon)),
+				new TreeItem<String>("스피커", new ImageView(spChildIcon)),
+				new TreeItem<String>("모니터", new ImageView(mnChildIcon)), };
+
+		root.setExpanded(true);
+		for (int i = 0; i < subRoot.length; i++) {
+			TreeItem<String> item = (TreeItem<String>) subRoot[i];
+			item.setExpanded(true);
+			root.getChildren().add(item);
+		}
+
+		// productTreeView.setCellFactory(treeView -> new MyTreeCell());
+		productTreeView.setRoot(root);
+
+		productTreeView.getSelectionModel().select(0);
 	}
-	
+
 	public void imageSel(MouseEvent event) {
-		// TODO Auto-generated method stub
 
 	}
-	
+
 	public void btnPageMove(ActionEvent event) {
-		// TODO Auto-generated method stub
 
 	}
 }
+
+//TODO 트리뷰 이벤트 적용 
+//class MyTreeCell extends TreeCell<String> {
+//
+//	public MyTreeCell() {
+//
+//		// 이벤트 처리..
+//
+//	}
+//
+//	@Override
+//	protected void updateItem(String item, boolean empty) {
+//		super.updateItem(item, empty);
+//		if (!empty) {
+//			setText(item);
+//			MyTreeItem treeItem = (MyTreeItem) getTreeItem();
+//			setGraphic(new ImageView(treeItem.getNormalIcon()));
+//		} else {
+//			setText(null);
+//			setGraphic(null);
+//		}
+//	}
+//}
