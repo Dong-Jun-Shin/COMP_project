dd03e74b8333c8d8ba1a609b688663c824be2f2d
Dongjun-Shin <tlsehdwns239@gmail.com>
2019-11-19 17:51:46 +0900

    19/11/19
    manageTraderTab - 검색 제외 구현 완료

--- Document/Code_source/COMP/src/controller/ManageTraderTabController.java
+++ Document/Code_source/COMP/src/controller/ManageTraderTabController.java
@@ -8,7 +8,6 @@ import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -20,7 +19,6 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import model.DataUtil;
-import model.ProductVO;
 import model.TraderDAO;
 import model.TraderVO;
 
@@ -46,13 +44,15 @@ public class ManageTraderTabController implements Initializable {
 	@FXML
 	private Button btnTRDelete;
 	@FXML
-	private ComboBox cmbTRSearchKey;
+	private ComboBox<String> cbxTRSearchKey;
 	@FXML
 	private TextField txtTRSearchValue;
 	@FXML
 	private Button btnTRSearch;
 	@FXML
 	private TableView<TraderVO> traderTableView;
+
+	String selectedTraderIndex;
 
 	private static ObservableList<TraderVO> traderDataList = FXCollections.observableArrayList();
 
@@ -64,6 +64,13 @@ public class ManageTraderTabController implements Initializable {
 		this.primaryStage = primaryStage;
 	}
 
+	public void setTRNum(MouseEvent event) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("TR_");
+		sb.append(trdao.getTraderCount());
+		txtTRNum.setText(sb.toString());
+	}
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		reset();
@@ -71,7 +78,7 @@ public class ManageTraderTabController implements Initializable {
 
 		//// 테이블뷰 컬럼이름 설정
 		List<String> title = DataUtil.fieldName(new TraderVO());
-		
+
 		// 설정을 받을 Table의 열
 		for (int i = 0; i < title.size(); i++) {
 			TableColumn<TraderVO, ?> columnName = traderTableView.getColumns().get(i);
@@ -83,138 +90,189 @@ public class ManageTraderTabController implements Initializable {
 		// 테이블에 항목 설정
 		traderTableView.setItems(traderDataList);
 
+		// 콤보박스 설정
+		setCbxList();
+
 		// 학과 전체 목록
 		traderTotalList();
 	}
 
 	public void btnTRInsert(ActionEvent event) {
-//		if(DataUtil.validityCheck(txtPNum.getText(), "제품번호")) {return;
-//		}else if(DataUtil.validityCheck(txtPName.getText(), "제품명")) {return;
-//		}else if(DataUtil.validityCheck(txtPQty.getText(), "수량")) {return;
-//		}else if(DataUtil.validityCheck(txtPPrice.getText(), "단가")) {return;
-//		}else if(DataUtil.validityCheck(txtPDate.getText(), "출시일")) {return;
-//		}else if(DataUtil.validityCheck(txtPImg.getText(), "이미지 파일")) {return;
-//		}else {
-//		
-//			ProductVO pvo = new ProductVO();
-//			pvo.setP_num(txtPNum.getText());
-//			pvo.setP_name(txtPName.getText());
-//			pvo.setP_qty(Integer.parseInt(txtPQty.getText()));
-//			pvo.setP_price(Integer.parseInt(txtPPrice.getText()));
-//			pvo.setP_size(txtPSize.getText());
-//			pvo.setP_grt(txtPGrt.getText());
-//			pvo.setP_date(txtPDate.getText());
-//			pvo.setP_img(txtPImg.getText());
-//		
-//		pddao.productInsert(pvo);
-//		}
-//		
+		boolean success = false;
 
+		try {
+			if (!DataUtil.validityCheck(txtTRNum.getText(), "거래처번호")) {
+				return;
+			} else if (!DataUtil.validityCheck(txtTRName.getText(), "거래처명")) {
+				return;
+			} else if (!DataUtil.validityCheck(txtTRPhone.getText(), "전화번호")) {
+				return;
+			} else if (txtTRAddress.getText().equals("")) {
+				DataUtil.validityCheck("", "주소를");
+			} else if (!DataUtil.validityCheck(txtTRBOwner.getText(), "계좌주")) {
+				return;
+			} else if (!DataUtil.bankNumCheck(txtTRBNum.getText(), "계좌번호")) {
+				return;
+			} else if (!DataUtil.validityCheck(txtTRBName.getText(), "계좌은행")) {
+				return;
+			} else {
+				TraderVO tvo = new TraderVO();
+				tvo.setTr_num(txtTRNum.getText().trim());
+				tvo.setTr_name(txtTRName.getText().trim());
+				tvo.setTr_phone(txtTRPhone.getText().trim());
+				tvo.setTr_add(txtTRAddress.getText().trim());
+				tvo.setTr_bowner(txtTRBOwner.getText().trim());
+				tvo.setTr_bnum(txtTRBNum.getText().trim());
+				tvo.setTr_bname(txtTRBName.getText().trim());
+
+				success = trdao.traderInsert(tvo);
+
+				if (success == true) {
+					DataUtil.showInfoAlert("거래처 등록 결과", "[" + txtTRName.getText() + "]의 수정을 성공하였습니다.");
+					traderTotalList();
+					reset();
+					editable(true);
+					setInsertBtn(true);
+				} else {
+					DataUtil.showInfoAlert("거래처 등록 결과", "거래처의 정보 등록에 문제가 있어 완료하지 못하였습니다.");
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void btnTRUpdate(ActionEvent event) {
+		boolean success = false;
+		try {
+			if (!DataUtil.validityCheck(txtTRNum.getText(), "거래처번호")) {
+				return;
+			} else if (!DataUtil.validityCheck(txtTRName.getText(), "거래처명")) {
+				return;
+			} else if (!DataUtil.validityCheck(txtTRPhone.getText(), "전화번호")) {
+				return;
+			} else if (txtTRAddress.getText().equals("")) {
+				DataUtil.validityCheck("", "주소를");
+			} else if (!DataUtil.validityCheck(txtTRBOwner.getText(), "계좌주")) {
+				return;
+			} else if (!DataUtil.bankNumCheck(txtTRBNum.getText(), "계좌번호")) {
+				return;
+			} else if (!DataUtil.validityCheck(txtTRBName.getText(), "계좌은행")) {
+				return;
+			} else {
 
-//		if(DataUtil.validityCheck(txtPNum.getText(), "제품번호")) {return;
-//		}else if(DataUtil.validityCheck(txtPName.getText(), "제품명")) {return;
-//		}else if(DataUtil.validityCheck(txtPQty.getText(), "수량")) {return;
-//		}else if(DataUtil.validityCheck(txtPPrice.getText(), "단가")) {return;
-//		}else if(DataUtil.validityCheck(txtPDate.getText(), "출시일")) {return;
-//		}else if(DataUtil.validityCheck(txtPImg.getText(), "이미지 파일")) {return;
-//		}else {
-//		
-//			ProductVO pvo = new ProductVO();
-//			pvo.setP_num(txtPNum.getText());
-//			pvo.setP_name(txtPName.getText());
-//			pvo.setP_qty(Integer.parseInt(txtPQty.getText()));
-//			pvo.setP_price(Integer.parseInt(txtPPrice.getText()));
-//			pvo.setP_size(txtPSize.getText());
-//			pvo.setP_grt(txtPGrt.getText());
-//			pvo.setP_date(txtPDate.getText());
-//			pvo.setP_img(txtPImg.getText());
-//		
-//			pddao.productUpdate(pvo);
-//			
-//		}
+				TraderVO tvo = new TraderVO();
+				tvo.setTr_num(txtTRNum.getText());
+				tvo.setTr_name(txtTRName.getText());
+				tvo.setTr_phone(txtTRPhone.getText());
+				tvo.setTr_add(txtTRAddress.getText());
+				tvo.setTr_bowner(txtTRBOwner.getText());
+				tvo.setTr_bnum(txtTRBNum.getText());
+				tvo.setTr_bname(txtTRBName.getText());
 
+				success = trdao.traderUpdate(tvo);
+
+				if (success == true) {
+					DataUtil.showInfoAlert("거래처 수정 결과", "[" + txtTRName.getText() + "]의 수정을 성공하였습니다.");
+					traderTotalList();
+					reset();
+					editable(true);
+					setInsertBtn(true);
+				} else {
+					DataUtil.showInfoAlert("거래처 수정 결과", "거래처의 정보 등록에 문제가 있어 수정을 완료하지 못하였습니다.");
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void btnTRDelete(ActionEvent event) {
-//		if(DataUtil.validityCheck(txtPNum.getText(), "제품번호")) {return;
-////		}else if(DataUtil.validityCheck(txtPName.getText(), "제품명")) {return;
-////		}else if(DataUtil.validityCheck(txtPQty.getText(), "수량")) {return;
-////		}else if(DataUtil.validityCheck(txtPPrice.getText(), "단가")) {return;
-////		}else if(DataUtil.validityCheck(txtPDate.getText(), "출시일")) {return;
-////		}else if(DataUtil.validityCheck(txtPImg.getText(), "이미지 파일")) {return;
-//		}else {
-//			
-//			ProductVO pvo = new ProductVO();
-//			pvo.setP_num(txtPNum.getText());
-//			pddao.productDelete(pvo);
-//		}
+		boolean success = false;
+		try {
+			TraderVO tvo = new TraderVO();
+			tvo.setTr_num(selectedTraderIndex);
+			success = trdao.traderDelete(tvo);
+		} catch (Exception e) {
+			System.out.println("btnDelete() = [ " + e + " ]");
+			e.printStackTrace();
+		}
+
+		if (success == true) {
+			DataUtil.showInfoAlert("거래처 삭제 결과", "[" + txtTRName.getText() + "]의 삭제를 성공하였습니다.");
+			traderTotalList();
+			reset();
+			editable(true);
+			setInsertBtn(true);
+		} else {
+			DataUtil.showInfoAlert("거래처 삭제 결과", "문제가 있어 삭제를 완료하지 못하였습니다.");
+		}
 	}
 
 	public void btnTRSearch(ActionEvent event) {
-//		switch(cmbPSearchKey.getValue().toString()) {
-//		case "제품번호" :
-//			pddao.getProductSelected("p_num",txtPSearchValue.getText());
-//			break;
-//	
-//		case "제품구분" :
-//			//TODO 입력받은 제품의 종류를 가져오도록 함
-//			switch(txtPSearchValue.getText()) {
-//				case "CPU" :
-//					pddao.getProductSelected("p_num", "CP_");
-//					break;
-//				case "" :
-//					
-//					break;
-//					
-//				default :
-//					
-//			}
-//			
-//			break;
-//		
-//		case "제품명" :
-//			
-//			pddao.getProductSelected("p_name",txtPSearchValue.getText());
-//			break;
-//			
-//		default :
-//			pddao.getProductTotalList();
-//			
-//			break;
-//			
-//			
-//			
-//		}
+		traderDataList.removeAll(traderDataList);
+		TraderVO tvo = null;
+		ArrayList<TraderVO> list;
+
+		try {
+			switch (cbxTRSearchKey.getValue().toString()) {
+			case "거래처번호":
+				list = trdao.getTraderSelected("tr_num", txtTRSearchValue.getText());
+				break;
+
+			case "거래처명":
+				list = trdao.getTraderSelected("tr_name", txtTRSearchValue.getText());
+				break;
+
+			case "계좌주":
+				list = trdao.getTraderSelected("tr_bowner", txtTRSearchValue.getText());
+				break;
+
+			default:
+				list = trdao.getTraderTotalList();
+				break;
+			}
+
+			for (int index = 0; index < list.size(); index++) {
+				// 결과 리스트에서 한 행을 가져다가 tvo에 대입
+				tvo = list.get(index);
+				// 한 행을 추가
+				traderDataList.add(tvo);
+			}
+		} catch (Exception e) {
+			System.out.println("traderTotalList() = [" + e.getMessage() + "]");
+		}
 	}
 
 	public void traderTableView(MouseEvent event) {
-//		ObservableList<ProductVO> list = FXCollections.observableArrayList();
-//		
-//		
-//		productTableView.setOnMouseClicked(new EventHandler<MouseEvent>() {
-//			@Override
-//			public void handle(MouseEvent event) {
-//				if(event.getClickCount() > 1) {
-//					//TODO list.get(int a) <- 선택한 값 가져오도록 설정, int값을 String값으로 가져오도록 함
-//					/*
-//					txtPNum.setText(list.get().getP_num());
-//					txtPName.setText(list.get().getP_name());
-//					txtPQty.setText(list.get().getP_qty());
-//					txtPPrice.setText(list.get().getP_price());
-//					txtPSize.setText(list.get().getP_size());
-//					txtPGrt.setText(list.get().getP_grt());
-//					txtPDate.setText(list.get().getP_date());
-//					txtPImg.setText(list.get().getP_img());
-//					*/
-//				}
-//			}
-//			
-//		});
-//		
+		if (event.getClickCount() == 2) {
+			TraderVO selectTrader = traderTableView.getSelectionModel().getSelectedItem();
+			if (selectTrader != null) {
+				selectedTraderIndex = selectTrader.getTr_num();
 
+				txtTRNum.setText(selectTrader.getTr_num());
+				txtTRName.setText(selectTrader.getTr_name());
+				txtTRPhone.setText(selectTrader.getTr_phone());
+				txtTRAddress.setText(selectTrader.getTr_add());
+				txtTRBOwner.setText(selectTrader.getTr_bowner());
+				txtTRBNum.setText(selectTrader.getTr_bnum());
+				txtTRBName.setText(selectTrader.getTr_bname());
+
+				setInsertBtn(false);
+				txtTRNum.setEditable(false);
+				txtTRName.setEditable(false);
+			}
+		}
+	}
+
+	private void editable(boolean bool) {
+		txtTRNum.setEditable(bool);
+		txtTRName.setEditable(bool);
+		txtTRPhone.setEditable(bool);
+		txtTRAddress.setEditable(bool);
+		txtTRBOwner.setEditable(bool);
+		txtTRBNum.setEditable(bool);
+		txtTRBName.setEditable(bool);
 	}
 
 	/**
@@ -245,16 +303,15 @@ public class ManageTraderTabController implements Initializable {
 	 * traderTotalList() : 테이블뷰 레코드 출력(거래처 전체 리스트)
 	 * 
 	 */
-	public void traderTotalList() {
+	private void traderTotalList() {
 		traderDataList.removeAll(traderDataList);
 		TraderVO svo = null;
 		ArrayList<TraderVO> list;
 
 		try {
 			list = trdao.getTraderTotalList();
-			int rowCount = list.size();
 
-			for (int index = 0; index < rowCount; index++) {
+			for (int index = 0; index < list.size(); index++) {
 				// 결과 리스트에서 한 행을 가져다가 svo에 대입
 				svo = list.get(index);
 				// 한 행을 추가
@@ -263,5 +320,9 @@ public class ManageTraderTabController implements Initializable {
 		} catch (Exception e) {
 			System.out.println("traderTotalList() = [" + e.getMessage() + "]");
 		}
+	}
+
+	public void setCbxList() {
+		cbxTRSearchKey.setItems(FXCollections.observableArrayList("거래처번호", "거래처명", "계좌주"));
 	}
 }
--- Document/Code_source/COMP/src/model/TraderDAO.java
+++ Document/Code_source/COMP/src/model/TraderDAO.java
@@ -9,157 +9,209 @@ import java.util.ArrayList;
 public class TraderDAO {
 
 	private static TraderDAO instance = null;
-	
+
 	private TraderDAO() {
-		
+
 	}
-	
+
 	/**
 	 * getConnection() : DB 연동 메소드
+	 * 
 	 * @return Connection
 	 * @throws Exception
 	 */
-	private Connection getConnection() throws Exception{
+	private Connection getConnection() throws Exception {
 		Connection con = DBUtill.getConnection();
 		return con;
 	}
-	
+
 	/**
 	 * getInstance() : 인스턴스 생성 메소드
+	 * 
 	 * @return TraderDAO
 	 */
 	public static TraderDAO getInstance() {
-		if(instance == null) {
+		if (instance == null) {
 			instance = new TraderDAO();
 		}
 		return instance;
 	}
-	
-	/**
-	 * getTraderTotalList() : 거래처 전체 조회 메소드
-	 * @return ArrayList<TraderVO>
-	 */
-	public ArrayList<TraderVO> getTraderTotalList(){
-		ArrayList<TraderVO> list = new ArrayList<TraderVO>();
+
+	public String getTraderCount() {
 		StringBuffer sql = new StringBuffer();
-		sql.append("SELECT tr_num, tr_name,tr_phone, tr_add, tr_bowner, tr_bnum, tr_bname, tr_reg FROM trader");
+		sql.append("SELECT NVL(LPAD(MAX(TO_NUMBER(LTRIM(SUBSTR(tr_num, ");
+		sql.append("4, 3), '0')))+1, 3, '0'), '001') AS traderCount ");
+		sql.append("FROM trader");
+
+		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		Connection con =null;
+		String serialNumber = "";
+
+		try {
+			con = getConnection();
+			pstmt = con.prepareStatement(sql.toString());
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				serialNumber = rs.getString("traderCount");
+			}
+		} catch (SQLException e) {
+			System.out.println("쿼리 getStudentCount() error = [" + e + " ]");
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("error = [" + e + " ]");
+			e.printStackTrace();
+		} finally {
+			try {
+				// 생성의 역순으로 닫기
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+				System.out.println("DB 연동 해제 error = [" + e + " ]");
+			}
+		}
+
+		return serialNumber;
+	}
+
+	/**
+	 * getTraderTotalList() : 거래처 전체 조회 메소드
+	 * 
+	 * @return ArrayList<TraderVO>
+	 */
+	public ArrayList<TraderVO> getTraderTotalList() {
+		ArrayList<TraderVO> list = new ArrayList<TraderVO>();
+		StringBuffer sql = new StringBuffer();
+		sql.append("SELECT tr_num, tr_name,tr_phone, tr_add, tr_bowner, tr_bnum, tr_bname, tr_reg FROM trader ");
+		sql.append("ORDER BY tr_num ASC");
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Connection con = null;
 		TraderVO tvo = null;
-		
-		try{
+
+		try {
 			con = getConnection();
 			pstmt = con.prepareStatement(sql.toString());
 			rs = pstmt.executeQuery();
-			tvo = new TraderVO();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
+				tvo = new TraderVO();
 				tvo.setTr_num(rs.getString("tr_num"));
 				tvo.setTr_name(rs.getString("tr_name"));
 				tvo.setTr_phone(rs.getString("tr_phone"));
 				tvo.setTr_add(rs.getString("tr_add"));
 				tvo.setTr_reg(rs.getDate("tr_reg").toString());
 				tvo.setTr_bowner(rs.getString("tr_bowner"));
-				tvo.setTr_bnum(rs.getString("tr_bnum"));;
+				tvo.setTr_bnum(rs.getString("tr_bnum"));
+				;
 				tvo.setTr_bname(rs.getString("tr_bname"));
 				list.add(tvo);
 			}
-			
-			}catch(SQLException sqle) {
-				System.out.println("[  getTraderTotalList()  ]    [ SQLException ]");
-				sqle.printStackTrace();
-			}catch(Exception e) {
-				System.out.println("[  getTraderTotalList()  ]    [ Unknown Exception ]");
-				e.printStackTrace();
-				
-			}finally {
-				try {
-					if(con != null) {
-						con.close();
-					}
-					if(pstmt != null) {
-						pstmt.close();
-					}
-					if(rs != null) {
-						rs.close();
-					}
-					
-				}catch(Exception e) {
-					System.out.println("[ getTraderTotalList()  ]    [ Closed Error ]");
-					e.printStackTrace();
+
+		} catch (SQLException sqle) {
+			System.out.println("[  getTraderTotalList()  ]    [ SQLException ]");
+			sqle.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("[  getTraderTotalList()  ]    [ Unknown Exception ]");
+			e.printStackTrace();
+
+		} finally {
+			try {
+				if (con != null) {
+					con.close();
 				}
-				
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (rs != null) {
+					rs.close();
+				}
+
+			} catch (Exception e) {
+				System.out.println("[ getTraderTotalList()  ]    [ Closed Error ]");
+				e.printStackTrace();
 			}
-		
+
+		}
+
 		return list;
 	}
-	
+
 	/**
 	 * getTraderSelected(String category, String searchWord) : 특정 거래처 정보 조회 메소드
+	 * 
 	 * @param (String) category : 검색 구분
 	 * @param (String) searchWord : 검색 키워드
 	 * @returnArrayList<TraderVO>
 	 */
-	public ArrayList<TraderVO> getTraderSelected(String category, String searchWord){
+	public ArrayList<TraderVO> getTraderSelected(String category, String searchWord) {
 		ArrayList<TraderVO> list = new ArrayList<TraderVO>();
 		StringBuffer sql = new StringBuffer();
 		sql.append("SELECT tr_num, tr_name,tr_phone, tr_add, tr_bowner, tr_bnum, tr_bname, tr_reg ");
-		sql.append("FROM trader WHERE "+category+" like "+searchWord);
+		sql.append("FROM trader WHERE " + category + " LIKE ?");
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		Connection con =null;
+		Connection con = null;
 		TraderVO tvo = null;
-		
-		try{
+
+		try {
 			con = getConnection();
 			pstmt = con.prepareStatement(sql.toString());
+			pstmt.setString(1, "%" + searchWord + "%");
 			rs = pstmt.executeQuery();
 			tvo = new TraderVO();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				tvo.setTr_num(rs.getString("tr_num"));
 				tvo.setTr_name(rs.getString("tr_name"));
 				tvo.setTr_phone(rs.getString("tr_phone"));
 				tvo.setTr_add(rs.getString("tr_add"));
 				tvo.setTr_reg(rs.getDate("tr_reg").toString());
 				tvo.setTr_bowner(rs.getString("tr_bowner"));
-				tvo.setTr_bnum(rs.getString("tr_bnum"));;
+				tvo.setTr_bnum(rs.getString("tr_bnum"));
+				;
 				tvo.setTr_bname(rs.getString("tr_bname"));
 				list.add(tvo);
 			}
-			
-			}catch(SQLException sqle) {
-				System.out.println("[  getTraderSelected(String category, String searchWord)  ]    [ SQLException ]");
-				sqle.printStackTrace();
-			}catch(Exception e) {
-				System.out.println("[  getTraderSelected(String category, String searchWord)  ]    [ Unknown Exception ]");
-				e.printStackTrace();
-				
-			}finally {
-				try {
-					if(con != null) {
-						con.close();
-					}
-					if(pstmt != null) {
-						pstmt.close();
-					}
-					if(rs != null) {
-						rs.close();
-					}
-					
-				}catch(Exception e) {
-					System.out.println("[ getTraderSelected(String category, String searchWord)  ]    [ Closed Error ]");
-					e.printStackTrace();
+
+		} catch (SQLException sqle) {
+			System.out.println("[  getTraderSelected(String category, String searchWord)  ]    [ SQLException ]");
+			sqle.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("[  getTraderSelected(String category, String searchWord)  ]    [ Unknown Exception ]");
+			e.printStackTrace();
+
+		} finally {
+			try {
+				if (con != null) {
+					con.close();
 				}
-				
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (rs != null) {
+					rs.close();
+				}
+
+			} catch (Exception e) {
+				System.out.println("[ getTraderSelected(String category, String searchWord)  ]    [ Closed Error ]");
+				e.printStackTrace();
 			}
-		
+
+		}
+
 		return list;
 	}
-	
+
 	/**
 	 * traderInsert(TraderVO tvo) : 거래처 정보 등록 메소드
+	 * 
 	 * @param tvo (TraderVO) : 등록할 거래처
 	 * @return boolean
 	 */
@@ -168,56 +220,57 @@ public class TraderDAO {
 		StringBuffer sql = new StringBuffer();
 		sql.append("INSERT INTO trader ");
 		sql.append("(tr_num, tr_name, tr_phone, tr_add, tr_bowner, tr_bnum, tr_bname)");
-		//번호
-		sql.append("'TR_'||LPAD(TO_CHAR(tr_num_seq.NEXTVAL),3,'0'),");
-		//거래처명, 전화번호, 주소, 계좌주, 계좌번호, 계좌은행 
-		sql.append("? , ? , ? , ? , ? , ? )");
+		// 번호
+		sql.append(" VALUES (?, ? , ? , ? , ? , ? , ? )");
+		// 거래처 번호, 거래처명, 전화번호, 주소, 계좌주, 계좌번호, 계좌은행
 		PreparedStatement pstmt = null;
 		Connection con = null;
-		
+
 		try {
 			con = getConnection();
 			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setString(1, tvo.getTr_name());
-			pstmt.setString(2, tvo.getTr_phone());
-			pstmt.setString(3, tvo.getTr_add());
-			pstmt.setString(4, tvo.getTr_bowner());
-			pstmt.setString(5, tvo.getTr_bnum());
-			pstmt.setString(6, tvo.getTr_bname());
+			pstmt.setString(1, tvo.getTr_num());
+			pstmt.setString(2, tvo.getTr_name());
+			pstmt.setString(3, tvo.getTr_phone());
+			pstmt.setString(4, tvo.getTr_add());
+			pstmt.setString(5, tvo.getTr_bowner());
+			pstmt.setString(6, tvo.getTr_bnum());
+			pstmt.setString(7, tvo.getTr_bname());
 			int i = pstmt.executeUpdate();
-			if(i ==1) {
+			if (i == 1) {
 				result = true;
 			}
-			
-		}catch(SQLException sqle) {
+
+		} catch (SQLException sqle) {
 			System.out.println("[  traderInsert(TraderVO tvo)  ] [  SQLException  ]");
 			sqle.printStackTrace();
 			result = false;
-		}catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("[  traderInsert(TraderVO tvo)  ] [  Exception  ]");
 			e.printStackTrace();
 			result = false;
-		}finally {
-			
+		} finally {
+
 			try {
-				if(pstmt != null) {
+				if (pstmt != null) {
 					pstmt.close();
 				}
-				if(con != null) {
+				if (con != null) {
 					con.close();
 				}
-			}catch(Exception e) {
+			} catch (Exception e) {
 				System.out.println("[  traderInsert(TraderVO tvo)  ] [  closed Error  ]");
 				e.printStackTrace();
 			}
-			
+
 		}
-		
+
 		return result;
 	}
-	
+
 	/**
 	 * traderUpdate(TraderVO tvo) : 거래처 정보 수정 메소드
+	 * 
 	 * @param tvo (TraderVO) : 수정할 거래처
 	 * @return boolean
 	 */
@@ -229,7 +282,7 @@ public class TraderDAO {
 		sql.append("WHERE tr_num = ?");
 		PreparedStatement pstmt = null;
 		Connection con = null;
-		
+
 		try {
 			con = getConnection();
 			pstmt = con.prepareStatement(sql.toString());
@@ -241,82 +294,82 @@ public class TraderDAO {
 			pstmt.setString(6, tvo.getTr_bname());
 			pstmt.setString(7, tvo.getTr_num());
 			int i = pstmt.executeUpdate();
-			if(i ==1) {
+			if (i == 1) {
 				result = true;
 			}
-			
-		}catch(SQLException sqle) {
+
+		} catch (SQLException sqle) {
 			System.out.println("[  traderUpdate(TraderVO tvo)  ] [  SQLException  ]");
 			sqle.printStackTrace();
 			result = false;
-		}catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("[  traderUpdate(TraderVO tvo)  ] [  Exception  ]");
 			e.printStackTrace();
 			result = false;
-		}finally {
-			
+		} finally {
+
 			try {
-				if(pstmt != null) {
+				if (pstmt != null) {
 					pstmt.close();
 				}
-				if(con != null) {
+				if (con != null) {
 					con.close();
 				}
-			}catch(Exception e) {
+			} catch (Exception e) {
 				System.out.println("[  traderUpdate(TraderVO tvo)  ] [  closed Error  ]");
 				e.printStackTrace();
 			}
-			
+
 		}
-		
+
 		return result;
 	}
-		
+
 	/**
 	 * traderDelete(TraderVO tvo) : 거래처 정보 삭제 메소드
+	 * 
 	 * @param tvo (TraderVO) : 삭제할 거래처
 	 * @return boolean
 	 */
 	public boolean traderDelete(TraderVO tvo) {
-		boolean result = false;
-		StringBuffer sql = new StringBuffer();
-		sql.append("DELETE FROM trader WHERE tr_num = ? ");
-		PreparedStatement pstmt = null;
+		boolean success = false;
 		Connection con = null;
-		
+		PreparedStatement pstmt = null;
+
 		try {
+			StringBuffer sql = new StringBuffer();
+			sql.append("DELETE FROM trader WHERE tr_num = ? ");
+
+
 			con = getConnection();
 			pstmt = con.prepareStatement(sql.toString());
 			pstmt.setString(1, tvo.getTr_num());
-			int i = pstmt.executeUpdate();
-			if(i ==1) {
-				result = true;
+			int cnt = pstmt.executeUpdate();
+			if (cnt == 1) {
+				success = true;
 			}
-			
-		}catch(SQLException sqle) {
+
+		} catch (SQLException sqle) {
 			System.out.println("[   traderDelete(TraderVO cvo)  ] [  SQLException  ]");
 			sqle.printStackTrace();
-			result = false;
-		}catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("[   traderDelete(TraderVO cvo)  ] [  Exception  ]");
 			e.printStackTrace();
-			result = false;
-		}finally {
-			
+		} finally {
 			try {
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(con != null) {
+				if (con != null) {
 					con.close();
 				}
-			}catch(Exception e) {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+			} catch (Exception e) {
 				System.out.println("[   traderDelete(TraderVO cvo)  ] [  closed Error  ]");
 				e.printStackTrace();
 			}
-			
+
 		}
-		return result;
+		return success;
 	}
-	
+
 }
